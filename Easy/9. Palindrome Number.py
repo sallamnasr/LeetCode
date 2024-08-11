383f3cb312f2f8b52d28a33bed6dce4272ed6a1c
sallamnasr <sallamnasr33@gmail.com>
2024-08-12 02:31:52 +0300

    changes for easy

---
+++ Easy/9. Palindrome Number.py
@@ -0,0 +1,23 @@
+# This soluion without convertion the number to string 
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            arr = []
+            while x != 0:
+                arr.append(x % 10)
+                x //= 10
+            return arr[::1] ==arr[::-1]
+        
+        
+# This soluion that convert the number to string
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            x = str(x)
+            return x[::1] == x[::-1]
