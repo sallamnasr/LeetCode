0d97a593e843c00995563f4914577b37dc94942b
sallamnasr <sallamnasr33@gmail.com>
2024-08-28 02:53:14 +0300

    Done

--- Easy/345. Reverse Vowels of a String.py
+++ Easy/345. Reverse Vowels of a String.py
@@ -15,35 +15,3 @@
 #                 j += 1
 #         return ''.join(s)
 
-
-def x(n, a: str, b: str):
-    if n == 2:
-        a = a[::-1]
-        b = b[::-1]
-        if a > b:
-            print('YES')
-            print(1, 2)
-        else:
-            print("NO")
-        return
-    if a[0] > b[0]:
-        print("YES")
-        print(2, 3)
-        return
-    for i in range(1, n):
-        if a[i] > b[i]:
-            print("YES")
-            print(1, i+1)
-            return
-    print("NO")
-    return
-
-
-if __name__ == '__main__':
-    t = 1 
-    # t = int(input())
-    for _ in range(t):
-        n = int(input())
-        a = input()
-        b = input()
-        x(n, a, b)
