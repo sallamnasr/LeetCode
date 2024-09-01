eb8499ab01c2eb60e58c7d2f76e77c1d24fac2a6
sallamnasr <sallamnasr33@gmail.com>
2024-09-01 21:21:10 +0300

    Ok

--- Binary Search Plan/Medium/153. Find Minimum in Rotated Sorted Array.py
+++ Binary Search Plan/Medium/153. Find Minimum in Rotated Sorted Array.py
@@ -1,17 +1,15 @@
 from typing import List
+
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l = 0
         r = len(nums) - 1
         while l < r:
             mid = l + (r-l)//2
-            if nums[mid] > nums[r] :
-                l = mid + 1 
-            else :
-                r = mid 
-        
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
         return nums[l]
-
-
-# time complixity is O(log(n))
-# space complixity is O(1)
