8dc96544132a94c464c8c952ffc6d1bb5bd2b81c
sallamnasr <sallamnasr33@gmail.com>
2024-09-01 21:11:51 +0300

    Done

--- Binary Search Plan/Medium/33. Search in Rotated Sorted Array.py
+++ Binary Search Plan/Medium/33. Search in Rotated Sorted Array.py
@@ -2,40 +2,38 @@
 # time : O(log(n))
 from typing import List
 class Solution:
-    def find_pivot(self, arr: List[int]):
-        l = 0
-        r = len(arr) - 1
+    def search(self, nums: List[int], target: int) -> int:
+        def min_idx(arr):
+            l, r = 0, len(arr) - 1
+            while l < r:
+                mid = l + (r-l)//2
+                if arr[mid] > arr[r]:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+        pivot = min_idx(nums)
+        l, r = 0, pivot - 1
         while l <= r:
             mid = l + (r-l)//2
-            if arr[mid] > arr[-1]:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return l
-
-    def search(self, nums: List[int], target: int) -> int:
-        end_start = self.find_pivot(nums)
-        nums1 = nums[:end_start]
-        nums2 = nums[end_start:]
-
-        l, r = 0, len(nums1) - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums1[mid] == target:
+            if nums[mid] == target:
                 return mid
-            elif nums1[mid] > target:
-                r = mid - 1
-            else:
+            elif nums[mid] < target:
                 l = mid + 1
+            else:
+                r = mid - 1
 
-        l, r = 0, len(nums2) - 1
+        l, r = pivot, len(nums) - 1
         while l <= r:
-            mid = l + (r - l) // 2
-            if nums2[mid] == target:
-                return mid + end_start
-            elif nums2[mid] > target:
-                r = mid - 1
-            else:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
                 l = mid + 1
-
+            else:
+                r = mid - 1
         return -1
+
+
+x = Solution()
+print(x.search([4, 5,6,7,0,1,2],0))
